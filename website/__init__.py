706f2107582e8763e297ddd2aedbbfcb2d53ed79
Bhaumik0 <bhaumikmedhekar24@gmail.com>
2024-05-18 17:56:58 +0530

    new changes

--- website/__init__.py
+++ website/__init__.py
@@ -8,38 +8,34 @@ DB_NAME = "database.db"
 
 
 def blog():
-    application = Flask(__name__)
-    application.config['SECRET_KEY'] = "bhaumik26"
-    application.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
-    db.init_app(application)
+    app = Flask(__name__)
+    app.config['SECRET_KEY'] = "bhaumik26"
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     from .views import views
     from .auth import auth
 
-    application.register_blueprint(views, url_prefix="/")
-    application.register_blueprint(auth, url_prefix="/")
+    app.register_blueprint(views, url_prefix="/")
+    app.register_blueprint(auth, url_prefix="/")
 
     from .models import User,Post
 
-    create_database(application)
+    create_database(app)
 
     login_manager = LoginManager()
     login_manager.login_view = "auth.login"
-    login_manager.init_app(application)
+    login_manager.init_app(app)
 
     @login_manager.user_loader
     def load_user(id):
         return User.query.get(int(id))
 
-    @application.route("/flask_crud")
-    def flask_crud():
-        return render_template("flask_crud.html")
 
+    return app
 
-    return application
-
-def create_database(application):
+def create_database(app):
     if not path.exists("website/"+DB_NAME):
-        with application.app_context():
+        with app.app_context():
             db.create_all()
             print("Created DataBase !")
